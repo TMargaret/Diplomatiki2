db82864c37b3a84d4232997fbc8e4484abba4f19
TMargaret <tmargaret@hotmail.com>
2017-06-05 14:13:27 +0300

    new classes

---
+++ Diplomatiki-SavingData/Greenfoot-Thesis-mainRoomAttempt/tuto/LevelsScreen.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LevelsScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelsScreen extends World
+{
+
+    /**
+     * Constructor for objects of class LevelsScreen.
+     * 
+     */
+    public LevelsScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
