071f2e56bdc46696ab8f484a8d1ff1c50f2d19db
TMargaret <tmargaret@hotmail.com>
2017-09-17 19:02:23 +0300

    quiz

--- Diplomatiki-SavingData/Greenfoot-Thesis-mainRoomAttempt/tuto/QuizPic.java
+++ Diplomatiki-SavingData/Greenfoot-Thesis-mainRoomAttempt/tuto/QuizPic.java
@@ -10,11 +10,25 @@ import java.util.List;
 
 public class QuizPic extends Actor
 {
+    GreenfootImage myImage = getImage();
     String question = "Ερώτηση";
+    String correctAnswer = "1234";
+    String givenAnswer = null;
     int questNum = 1;
+    Button btn1 = new Button();
+    Button btn2 = new Button();
+    Button btn3 = new Button();
+    Button btn4 = new Button();
 
     public QuizPic(){
 
+    }
+    protected void addedToWorld(World w)
+    {
+        getWorld().addObject(btn1, getImage().getWidth() - btn1.getImage().getWidth()-btn1.getImage().getWidth()/2, getImage().getHeight()- (btn1.getImage().getHeight()*3));
+        getWorld().addObject(btn2, getImage().getWidth() - btn2.getImage().getWidth()-btn2.getImage().getWidth()/2, getImage().getHeight()- btn1.getImage().getHeight()*2);
+        getWorld().addObject(btn3, getImage().getWidth() - btn3.getImage().getWidth()-btn3.getImage().getWidth()/2 , getImage().getHeight()- btn1.getImage().getHeight());
+        getWorld().addObject(btn4, getImage().getWidth() - btn4.getImage().getWidth()-btn4.getImage().getWidth()/2, getImage().getHeight());
     }
 
     /**
@@ -24,17 +38,22 @@ public class QuizPic extends Actor
     public void act() 
     {
         drawText();
- 
+
     }
-    
+
     public void drawText(){
+        
         String text = "";
         switch (questNum){
             case 1:
             text = quiz1();
+            answerBtn1();
+            getAnswer();          
             break;
             case 2:
             text = quiz2();
+            answerBtn2();
+            getAnswer();
             break;
             case 3:
             text = quiz3();
@@ -51,39 +70,92 @@ public class QuizPic extends Actor
         getImage().drawString(text, 30, 100);        
     }
     
+    public void getAnswer(){
+        if (Greenfoot.mouseClicked(btn1)){
+            givenAnswer += 1;
+            setBackground().
+        }
+        if (Greenfoot.mouseClicked(btn2)){
+            givenAnswer += 2;
+        }
+        if (Greenfoot.mouseClicked(btn3)){
+            givenAnswer += 3;
+        }
+        if (Greenfoot.mouseClicked(btn4)){
+            givenAnswer += 4;
+        }
+
+
+    }
+
     public String quiz1(){
         return "Με τη γραμμή κώδικα:\n"
-                +"new Dragon();\n"
-                +"Τι από τα παρακάτω θα συμβεί;"
-                +"";
+        +"new Dragon();\n"
+        +"Τι από τα παρακάτω θα συμβεί;"
+        +"";
+    }
+
+    public String quiz2(){
+        return "Με τη γραμμή κώδικα:\n"
+        +"new Dragon();\n"
+        +"Τι από τα παρακάτω θα συμβεί;"
+        +"";
+    }
+
+    public String quiz3(){
+        return "Με τη γραμμή κώδικα:\n"
+        +"new Dragon();\n"
+        +"Τι από τα παρακάτω θα συμβεί;"
+        +"";
+    }
+
+    public String quiz4(){
+        return "Με τη γραμμή κώδικα:\n"
+        +"new Dragon();\n"
+        +"Τι από τα παρακάτω θα συμβεί;"
+        +"";
+    }
+
+    public String quiz5(){
+        return "Με τη γραμμή κώδικα:\n"
+        +"new Dragon();\n"
+        +"Τι από τα παρακάτω θα συμβεί;"
+        +"";
+    }
+
+    public void answerBtn1(){
+        btn1.setTitle("Τίποτα");
+        btn2.setTitle("Τίποτα");
+        btn3.setTitle("Τίποτα");
+        btn4.setTitle("Τίποτα");
     }
     
-        public String quiz2(){
-        return "Με τη γραμμή κώδικα:\n"
-                +"new Dragon();\n"
-                +"Τι από τα παρακάτω θα συμβεί;"
-                +"";
+    public void answerBtn2(){
+        btn1.setTitle("Τίποτα");
+        btn2.setTitle("Τίποτα");
+        btn3.setTitle("Τίποτα");
+        btn4.setTitle("Τίποτα");
+    }
+    public void answerBtn3(){
+        btn1.setTitle("Τίποτα");
+        btn2.setTitle("Τίποτα");
+        btn3.setTitle("Τίποτα");
+        btn4.setTitle("Τίποτα");
+    }
+    public void answerBtn4(){
+        btn1.setTitle("Τίποτα");
+        btn2.setTitle("Τίποτα");
+        btn3.setTitle("Τίποτα");
+        btn4.setTitle("Τίποτα");
+    }
+    public void answerBtn5(){
+        btn1.setTitle("Τίποτα");
+        btn2.setTitle("Τίποτα");
+        btn3.setTitle("Τίποτα");
+        btn4.setTitle("Τίποτα");
     }
     
-        public String quiz3(){
-        return "Με τη γραμμή κώδικα:\n"
-                +"new Dragon();\n"
-                +"Τι από τα παρακάτω θα συμβεί;"
-                +"";
-    }
     
-        public String quiz4(){
-        return "Με τη γραμμή κώδικα:\n"
-                +"new Dragon();\n"
-                +"Τι από τα παρακάτω θα συμβεί;"
-                +"";
-    }
     
-        public String quiz5(){
-        return "Με τη γραμμή κώδικα:\n"
-                +"new Dragon();\n"
-                +"Τι από τα παρακάτω θα συμβεί;"
-                +"";
-    }
 }
 
