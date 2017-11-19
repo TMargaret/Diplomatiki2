8d75ea849ae033e3aacbc495ea56516b58db5797
TMargaret <tmargaret@hotmail.com>
2017-11-20 00:12:02 +0200

    spider-ant

---
+++ Diplomatiki-SavingData/Greenfoot-Thesis-mainRoomAttempt/tuto/Spider.java
@@ -0,0 +1,48 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Spider extends ScrollingEnemy
+{
+    GreenfootImage spider = new GreenfootImage("ant.png");
+    int img_cell =64;
+    final int IMG_WIDTH = spider.getWidth()/5;
+    final int IMG_HEIGHT = spider.getHeight()/5;
+    private final static int SPEEDVARIATION = 3;
+    private final static int SPEEDCHANGECHANCE = 20;
+    int count = 0;
+    int count2 = 0;
+
+    public Spider() {
+        setImage(SpriteSheet.getSprite(spider, 0,  img_cell, img_cell, img_cell*2, IMG_WIDTH, IMG_HEIGHT));
+    }
+
+    protected void reaction() {
+        if(Greenfoot.getRandomNumber(1000) < SPEEDCHANGECHANCE){
+            speedX = Greenfoot.getRandomNumber(SPEEDVARIATION)-1;           
+        }else{
+            speedX = speedX;
+            if (speedX>0){
+                spiderAnimation(1);
+            }
+            if (speedX<0){
+                spiderAnimation(3);
+            }
+            if (speedX==0){
+                setImage(SpriteSheet.getSprite(spider, 0,  img_cell, img_cell, img_cell*2, IMG_WIDTH, IMG_HEIGHT));
+            }
+        }
+        speedY = Greenfoot.getRandomNumber(1000) < SPEEDCHANGECHANCE ?
+            Greenfoot.getRandomNumber(SPEEDVARIATION)-1 : speedY; 
+    }
+
+    public void spiderAnimation(int y){
+        System.out.println(y);
+        for (int i=1;i<=4;i++){
+            count++;
+            if (count>50){
+                setImage(SpriteSheet.getSprite(spider, img_cell*(i-1), img_cell*y, img_cell*i, img_cell*(y+1), IMG_WIDTH, IMG_HEIGHT));
+                count = 0;
+            }
+        }
+
+    }
+}
